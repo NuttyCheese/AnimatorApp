8dac4ca2a0e668508e4b717ea9cabd1f8957db67
BorisPavlov <pbd_b@mail.ru>
2022-05-06 14:49:42 +0300

    Initial Commit

---
+++ AnimatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatorApp
+//
+//  Created by Борис Павлов on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
